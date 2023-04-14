a1c84c85e9c04a8fd15953488905a7d3a3a4edbf
Nestor Ulloa <nestoreumma@gmail.com>
2023-04-14 00:01:26 -0600

    ultimas modificaciones solicitadas

--- sale_order_medical_information/models/stock_picking.py
+++ sale_order_medical_information/models/stock_picking.py
@@ -4,14 +4,15 @@ from odoo import fields, models, api, _
 class StockPickingDoc(models.Model):
     _inherit = 'stock.picking'
 
-    partner_paciente = fields.Char(string="Paciente", readonly=True)
-    partner_doctor = fields.Many2one('res.partner', string="Doctor", readonly=True)
-    fecha_operacion = fields.Datetime(string="Fecha operacion", readonly=True)
+    partner_paciente = fields.Char(string="Paciente")
+    partner_doctor = fields.Many2one('res.partner', string="Doctor")
+    fecha_operacion = fields.Datetime(string="Fecha operacion")
     tipo_venta = fields.Selection(
-        selection=[('ventadirecta', 'VENTA DIRECTA'),
+        selection=[
+                   ('ventadirecta', 'VENTA DIRECTA'),
                    ('transito', 'TRANSITO'),
                    ('asistenciacirugia', 'ASISTENCIA CIRUGIA'),
                    ('consignacion', 'CONSIGNACION')],
-        string='Tipo Venta', required=True, default='ventadirecta', readonly=True)
+        string='Tipo Venta')
 
 
