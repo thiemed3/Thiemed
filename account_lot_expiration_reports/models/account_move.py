32da769201b2110613b76cf5831619dacc3eefaa
Nestor Ulloa <nestoreumma@gmail.com>
2023-04-22 18:10:41 -0600

    modificacion en el modulo de account_lote_expiration, se cambia la funcion y se obtiene unicamente el producto por el stock_move

--- account_lot_expiration_reports/models/account_move.py
+++ account_lot_expiration_reports/models/account_move.py
@@ -5,12 +5,7 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     sale_order_id = fields.Many2one('sale.order', string='Pedido', compute='_compute_sale_order_id', store=True)
-
-    # def buscar_pedido(self):
-    #     for rec in self:
-    #         if rec.invoice_origin:
-    #             sale_order = self.env['sale.order'].search([('name', '=', rec.invoice_origin)])
-    #             return sale_order
+    cantidad_lote = fields.Char(string='Cantidad Lote', compute='_compute_cantidad_lote')
 
     @api.depends('invoice_origin')
     def _compute_sale_order_id(self):
--- account_lot_expiration_reports/models/sale_order_line.py
+++ account_lot_expiration_reports/models/sale_order_line.py
@@ -1,8 +1,10 @@
 from odoo import fields, models, api
-
+import json
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
+
+    cantidad_lote = fields.Char(string='Cantidad Lote', compute='_compute_cantidad_lote')
 
 
     def _prepare_invoice_line(self, **optional_values):
@@ -11,5 +13,43 @@ class SaleOrderLine(models.Model):
             if lot.product_id.id == self.product_id.id:
                 res.update({'lot_id': lot.lot_id.id})
         return res
+
+    def _compute_cantidad_lote(self):
+        for rec in self:
+            cantidad_lote = {}
+
+            move_lines = rec.move_ids.mapped('move_line_ids')
+            # RECORRER LOS MOVIENMIENTOS DE LA LINEA DE VENTA Y OBTENER EL LOTE Y CANTIDAD.
+            for move_line in move_lines:
+                if move_line.lot_id:
+                    if move_line.lot_id.name in cantidad_lote:
+                        if move_line.picking_id.picking_type_code == 'outgoing':
+                            cantidad_lote[move_line.lot_id.name] += move_line.qty_done
+                        if move_line.picking_id.picking_type_code == 'incoming':
+                            cantidad_lote[move_line.lot_id.name] -= move_line.qty_done
+
+                    else:
+                        cantidad_lote[move_line.lot_id.name] = move_line.qty_done
+
+            # Crea una lista de las llaves que se deben eliminar
+            keys_to_remove = []
+            for key, value in cantidad_lote.items():
+                if value == 0 or value == 0.0:
+                    keys_to_remove.append(key)
+
+            # Elimina las llaves del diccionario
+            for key in keys_to_remove:
+                del cantidad_lote[key]
+
+            rec.cantidad_lote = json.dumps(cantidad_lote)
+
+
+
+
+
+
+
+
+
 
 
