5c6d2e777854ed13bc20a68644407170557a5f01
Nestor Ulloa <nestoreumma@gmail.com>
2023-04-26 00:59:34 -0600

    modificaciones en reportes de entregados y vuelve a estar obligatorio el campo tipo de venta

--- sale_order_medical_information/models/sale_order.py
+++ sale_order_medical_information/models/sale_order.py
@@ -12,7 +12,7 @@ class SaleOrder(models.Model):
                    ('transito', 'TRANSITO'),
                    ('asistenciacirugia', 'ASISTENCIA CIRUGIA'),
                    ('consignacion', 'CONSIGNACION')],
-        string='Tipo Venta')
+        string='Tipo Venta', required=True)
 
     def _action_confirm(self):
         res = super(SaleOrder, self)._action_confirm()
