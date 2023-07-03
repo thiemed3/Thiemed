811ab9360eb0186f8cec0a836d1148ecc239e3e9
Esteban Arroyo M <esteban.arroyo@gmail.com>
2023-07-03 18:09:05 -0400

    agrega referencias cruzadas de documento

---
+++ l10n_cl_picking_references/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': "Referencias en stock",
+    'category': 'stock',
+    'version': '14.0.1.0',
+    'author': 'Pedro Arroyo - Tierranube',
+    'description': """
+
+    """,
+    'summary': """""",
+    'depends': ['base', 'l10n_cl', 'stock'],
+    'currency': 'CLP',
+    'license': 'OPL-1',
+    'website': "",
+    'data': [
+        'views/stock_picking.xml'
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+
--- l10n_cl_picking_references/models/stock_picking.py
+++ l10n_cl_picking_references/models/stock_picking.py
@@ -4,18 +4,6 @@ from odoo import fields, models, api, _
 class StockPickingDoc(models.Model):
     _inherit = 'stock.picking'
 
-    partner_paciente = fields.Char(string="Paciente")
-    partner_doctor = fields.Many2one('res.partner', string="Doctor")
-    fecha_operacion = fields.Datetime(string="Fecha operacion")
-    asistente_ciguria = fields.Many2one('res.users', string="Asistente cirugia", default=lambda self: self.env.user.id)
-    tipo_venta = fields.Selection(
-        selection=[
-                   ('ventadirecta', 'VENTA DIRECTA'),
-                   ('transito', 'TRANSITO'),
-                   ('asistenciacirugia', 'ASISTENCIA CIRUGIA'),
-                   ('consignacion', 'CONSIGNACION')],
-        string='Tipo Venta')
-
     l10n_cl_reference_ids = fields.One2many('l10n_cl.account.invoice.reference', 'picking_id', readonly=True,
                                             states={'draft': [('readonly', False)]}, string='Reference Records')
 
