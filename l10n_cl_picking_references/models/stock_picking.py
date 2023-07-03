b230f937aa648a7b1291fa97f275d3c0879f5a99
Esteban Arroyo M <esteban.arroyo@gmail.com>
2023-07-03 18:33:43 -0400

    Update stock_picking.py

--- l10n_cl_picking_references/models/stock_picking.py
+++ l10n_cl_picking_references/models/stock_picking.py
@@ -4,8 +4,7 @@ from odoo import fields, models, api, _
 class StockPickingDoc(models.Model):
     _inherit = 'stock.picking'
 
-    l10n_cl_reference_ids = fields.One2many('l10n_cl.account.invoice.reference', 'picking_id', readonly=True,
-                                            states={'draft': [('readonly', False)],'waiting':[('readonly', False)]}, string='Reference Records')
+    l10n_cl_reference_ids = fields.One2many('l10n_cl.account.invoice.reference', 'picking_id', readonly=False, string='Reference Records')
 
 class AccountInvoiceReference(models.Model):
     _inherit = 'l10n_cl.account.invoice.reference'
