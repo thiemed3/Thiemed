25fe461d6e5f44397d7ab96bf1cdcab465a91036
nestor <ulloa.nestor22@gmail.com>
2023-07-20 14:48:44 -0600

    Modificaciones solicitadas, se agrega campo de fecha y se modifica el archivo del dte, se agrega campo de especialista en los ajustes de la venta.

--- sale_order_medical_information/models/sale_order.py
+++ sale_order_medical_information/models/sale_order.py
@@ -7,9 +7,9 @@ class SaleOrder(models.Model):
     partner_paciente = fields.Char(string="Paciente")
     partner_doctor = fields.Many2one('res.partner', string="Doctor")
     fecha_operacion = fields.Datetime(string="Fecha operacion")
-    # asistente_cirugia = fields.Many2one('res.users', string="Asistente cirugia", default=lambda self: self.env.user)
-    asistente_cirugia = fields.Many2one('res.users', string="Asistente cirugia",
-                                        related='company_id.partner_especialista_id', store=True, readonly=False)
+    asistente_cirugia = fields.Many2one('res.users', string="Asistente cirugia", default=lambda self: self.env.user)
+    # asistente_cirugia = fields.Many2one('res.users', string="Asistente cirugia",
+    #                                     related='company_id.partner_especialista_id', store=True, readonly=False)
     tipo_venta = fields.Selection(
         selection=[('ventadirecta', 'VENTA DIRECTA'),
                    ('transito', 'TRANSITO'),
