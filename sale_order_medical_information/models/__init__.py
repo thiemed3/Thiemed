3977a29dfca65a9a3d2103d2bece54e710645336
nestor <ulloa.nestor22@gmail.com>
2023-07-20 14:52:04 -0600

    Modificaciones solicitadas, se agrega campo de fecha y se modifica el archivo del dte, se agrega campo de especialista en los ajustes de la venta.

--- sale_order_medical_information/models/__init__.py
+++ sale_order_medical_information/models/__init__.py
@@ -1,5 +1,6 @@
+from . import res_config_settings
+from . import res_company
 from . import sale_order
 from . import stock_picking
 from . import account_move
-from . import res_config_settings
-from . import res_company
+
