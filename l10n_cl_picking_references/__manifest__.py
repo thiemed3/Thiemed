e32baeb9cd29bea8a96e2e985f5170741635ef3e
Esteban Arroyo M <esteban.arroyo@gmail.com>
2024-08-20 23:52:11 -0400

    [FIX] referencias de picking

--- l10n_cl_picking_references/__manifest__.py
+++ l10n_cl_picking_references/__manifest__.py
@@ -14,7 +14,7 @@
         'views/stock_picking.xml',
         'views/report_delivery_guide.xml',
         'views/dte_template.xml',
-        #'views/sale_order.xml',
+        'views/sale_order.xml',
     ],
     'demo': [],
     'installable': True,
