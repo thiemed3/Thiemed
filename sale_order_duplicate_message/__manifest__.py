a5af0899b9f55442fefdef39d224a904b23318f8
Soporte Tierra Nube <88048271+soportetierranube@users.noreply.github.com>
2026-03-31 16:17:59 -0300

    Add files via upload

---
+++ sale_order_duplicate_message/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Sale Order Duplicate Chatter",
+    'summary': """Modulo para avisar por el chatter de donde se duplico el pedido de venta""",
+    'description': "",
+    'author': "Tierranube(Matias VA)",
+    'website': "http://www.tierranube.cl",
+    'license': 'LGPL-3',
+    'category': 'sale',
+    'version': '18.0.0.1',
+    'depends': ['sale'],
+    'data': [
+        'data/duplication_message.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
