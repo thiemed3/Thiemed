b53abeeb16c09740a4df8b19e64406db457abc2f
Thiemed <64040250+thiemed3@users.noreply.github.com>
2026-04-08 15:40:25 -0400

    [FIX] ticket #2514
    Alberto Toro (Thiemed): Init was edited to avoid using the stock_landed_cost model, because it's causing errors in the landed costs.

--- as_thimed_invoice/models/__init__.py
+++ as_thimed_invoice/models/__init__.py
@@ -3,6 +3,6 @@ from . import sale_order_referencias
 from . import sii
 from . import as_pricelist_item
 from . import as_account_move
-from . import stock_landed_cost
+# from . import stock_landed_cost
 from . import as_res_config
 from . import as_stock_production_lot
