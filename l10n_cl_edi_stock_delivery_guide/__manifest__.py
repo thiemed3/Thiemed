f1990edcab5fe5fe436b7c678f96a2950691d854
soportetierranube <88048271+soportetierranube@users.noreply.github.com>
2026-08-14 12:58:49 -0400

    [MIG] l10n_cl_edi_stock_delivery_guide
    Se migra modulo l10n_cl_edi_stock_delivery_guide a odoo 19

--- l10n_cl_edi_stock_delivery_guide/__manifest__.py
+++ l10n_cl_edi_stock_delivery_guide/__manifest__.py
@@ -5,7 +5,7 @@
     'author': "Tierranube",
     'website': "https://www.tierranube.cl",
     'category': 'Accounting/Localizations/EDI',
-    'version': '18.0.0.1',
+    'version': '19.0.0.1',
     'depends': ['l10n_cl_edi_stock'],
     'data': [
         'views/report_delivery_guide.xml',
