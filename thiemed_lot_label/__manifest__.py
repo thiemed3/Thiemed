9911e672f5899b2ed09e8cd77a549e45827da717
Thiemed <64040250+thiemed3@users.noreply.github.com>
2026-09-07 14:57:24 -0300

    Modulo de etiquetas
    Carga modulo thiemed_lot_label

---
+++ thiemed_lot_label/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Thiemed - Impresión múltiple de etiquetas de lote",
+    "version": "18.0.1.0.0",
+    "category": "Inventory/Inventory",
+    "license": "LGPL-3",
+    "depends": ["stock"],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizard/lot_label_wizard_views.xml"
+    ],
+    "installable": True
+}
