e9cafe511d74913355d66579a36d660ee2225fa9
Esteban Arroyo M <esteban.arroyo@gmail.com>
2026-09-23 10:23:46 -0300

    [FIX] Cambio version modulo lot label

--- thiemed_lot_label/__manifest__.py
+++ thiemed_lot_label/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Thiemed - Impresión múltiple de etiquetas de lote",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Inventory/Inventory",
     "license": "LGPL-3",
     "depends": ["stock"],
